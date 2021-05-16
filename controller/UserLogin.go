d63bec757a83d8085b0b56008f424135cf63348a
zhongwq <wiqi.zhong@gmail.com>
2021-05-16 22:33:35 +0800

    home

--- controller/UserLogin.go
+++ controller/UserLogin.go
@@ -10,6 +10,7 @@ type UserLoginService struct {
 }
 
 func (*UserLoginService) UserLogin(login *reqs.Login) interface{} {
+	//log.Info("abdd")
 	fmt.Println("++++++++++++++++++++++++++ call userLogin  userName = " + login.User)
 	login.User = "hello:" + login.User
 	return login
